ca004784b9229e954fcf87771bbf97d020513dbd
Fernando González Rivero <fernando.gonzalezr@upb.edu.co>
2025-03-05 08:27:46 -0500

    Eliminar funcionalidad de contador

--- src/App.jsx
+++ src/App.jsx
@@ -13,34 +13,13 @@ const tiposUsuario = {
 
 
 function App() {
-  const [count, setCount] = useState(0);
   const [role, setRole] = useState(null);
   const possible_roles = ["admin", "cliente", "invitado"];
 
   return (
     <>
-    <Header setRole={setRole} possibleRoles={tiposUsuario}/>
-    <UserPanel tiposUsuario={tiposUsuario} selectedTipoUsu={role}/>
-    {/*
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>UwU</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          numerito es {count}
-        </button>
-        
-      </div>
-      <p className="read-the-docs">
-        Hola Mundo, este es nuestro proyecto de plataformas.
-      </p>
-  */}
+			<Header setRole={setRole} possibleRoles={tiposUsuario}/>
+	    <UserPanel tiposUsuario={tiposUsuario} selectedTipoUsu={role}/>
     </>
   )
 }
