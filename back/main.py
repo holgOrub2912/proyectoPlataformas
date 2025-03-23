1dfb6d4ba81954ad4f83db97e8e0b32ccf4693d3
Fernando González Rivero <fernando.gonzalezr@upb.edu.co>
2025-03-23 12:21:11 -0500

    Add create and view single user endpoints

--- back/main.py
+++ back/main.py
@@ -1,5 +1,21 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from models import Usuario, engine
+from sqlmodel import Session, select
 
 app = FastAPI()
+session = Session(engine)
+
+@app.get("/api/usuarios/{id}")
+async def get_user(id: int):
+    return session.exec(select(Usuario)
+                        .where(Usuario.id == id)).first()
+
+@app.post("/api/usuarios")
+async def create_user(usuario: Usuario):
+    session.add(usuario)
+    session.commit()
+    return usuario
+
 app.mount("/", StaticFiles(directory="../front/dist"), name="frontend")
+
