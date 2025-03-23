17bbacafe16d4b3b8c4214a6d8b8e5f6136ee084
Fernando González Rivero <fernando.gonzalezr@upb.edu.co>
2025-03-23 12:23:04 -0500

    Add get_users endpoint

--- back/main.py
+++ back/main.py
@@ -11,6 +11,13 @@ async def get_user(id: int):
     return session.exec(select(Usuario)
                         .where(Usuario.id == id)).first()
 
+@app.get("/api/usuarios")
+async def get_users():
+    return [
+        usuario for usuario in
+        session.exec(select(Usuario))
+    ]
+
 @app.post("/api/usuarios")
 async def create_user(usuario: Usuario):
     session.add(usuario)
