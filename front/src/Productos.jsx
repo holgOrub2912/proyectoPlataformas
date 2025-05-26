132a21b85e889a65e8150761830b47f85513a9fe
Fernando González Rivero <fernando.gonzalezr@upb.edu.co>
2025-05-25 19:19:35 -0500

    Style Productos

--- front/src/Productos.jsx
+++ front/src/Productos.jsx
@@ -7,7 +7,7 @@ import Table from 'rc-table';
 const Productos = ({}) => {
   const {user, token} = useAuth();
   const [products, setProducts] = useState([]);
-  const [addingProduct, setAddingProduct] = useState(false);
+  const [addingProduct, setAddingProduct] = useState(true);
   const [newProductName, setNewProductName] = useState('');
   const [newProductPrice, setNewProductPrice] = useState('');
 
@@ -31,13 +31,19 @@ const Productos = ({}) => {
     getInfo('productos', setAndFormatProducts);
   }, []);
 
+  const inputClasses = "py-1 px-5 my-1 mx-2 focus:border-0 border-b-2 border-gray-700 outline-lime-100";
+
   const newProductRow = products.length > 0
   ? {id: -1,
      nombre: <input value={newProductName}
+                    placeholder="Nombre"
+                    className={inputClasses}
                     onChange={e => setNewProductName(e.target.value)}
                     type='text'/>,
      precio: <input value={newProductPrice}
                     onChange={e => setOnlyNumbers(e.target.value)}
+                    placeholder="Precio"
+                    className={inputClasses}
                     type='text'/>}
   : {};
 
@@ -50,14 +56,19 @@ const Productos = ({}) => {
     {
       title: 'Precio',
       dataIndex: 'precio',
+      className: 'py-1',
       key: 'precio',
     },
   ];
 
-  return <div>
-    <Table rowKey={'id'}
-           columns={columns}
-           data={[...products, addingProduct && newProductRow]}/>
+  return <div className="flex flex-col items-center">
+    <Table
+      rowKey={'id'}
+      className="text-center my-3"
+      columns={columns}
+      data={[...products, addingProduct && newProductRow]}
+      rowClassName={(c,i) => "border-t-1 " + ((i % 2 == 1) ? " bg-gray-100" : "")}
+    />
     {user && user.role == 1 && (addingProduct
       ? <button onClick={saveProduct}>Guardar Producto</button>
       : <button onClick={() => setAddingProduct(true)}>Añadir Producto</button>)
